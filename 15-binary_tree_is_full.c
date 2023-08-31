9bde1e3507cb1ce50eb0f0fc5f10c213d4c65bfc
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-08-31 11:18:13 -0700

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,10 +7,16 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left, right;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+	return (left && right);
 }
